99f64d9d98f94a5e2ffb15944a3c06c72ec524ba
Mas'udD0220024 <73059550+masud832@users.noreply.github.com>
2021-03-02 21:47:25 +0700

    Update Main.java

--- src/main/java/fosalgo/Main.java
+++ src/main/java/fosalgo/Main.java
@@ -13,7 +13,15 @@ public class Main {
         int n = sc.nextInt();
         
         //TULISKAN KODEMU DIBAWAH INI
-        
+        String[] namaBulan = {"Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"};
+        if (n >= 1 && n <= 12) {
+            n--;
+            System.out.println("Output----");
+            System.out.println(namaBulan[n]);
+            System.out.println(namaBulan[(++n) % 12]);
+            System.out.println(namaBulan[(++n) % 12]);
+            System.out.println(namaBulan[(++n) % 12]);
+        }
         
         
     }
